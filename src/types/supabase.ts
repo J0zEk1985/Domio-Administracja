87a6fd7292240599ffe9743cda65fb1676be751b
Marcin Józefiak <jozefiakmar@gmial.com>
2026-09-08 23:01:31 +0200

    dodawanie budynków w każdym module osobno

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -235,9 +235,9 @@ export type Database = {
           id: string
           instruction_notes: string | null
           is_active_in_serwis: boolean | null
-          is_admin_active: boolean | null
-          is_cleaning_active: boolean | null
-          is_maintenance_active: boolean | null
+          is_admin_active: boolean
+          is_cleaning_active: boolean
+          is_maintenance_active: boolean
           allow_anonymous_qr_reports: boolean
           issue_qr_token: string | null
           latitude: number | null
